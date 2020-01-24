936368b30babe900c00a73f0e2c441abf7b30d76
Ben Thayer <ben@benthayer.com>
2020-01-24 13:23:00 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='Emailer',
+    version='0.1',
+    description='Simple emailer',
+    author='Ben Thayer',
+    author_email='ben@benthayer.com',
+    url='https://github.com/benthayer/emailer',
+    packages=['sendemail'],
+)
