47be91766b6f55e5b964ec5d88f0dd20fafb81a4
apps.benthayer.com <admin@apps.benthayer.com>
2020-03-11 15:17:55 -0500

    Added more options for send function

--- emailer.py
+++ emailer.py
@@ -3,25 +3,38 @@ import smtplib
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 from email.utils import formatdate
 
 
-def send(subject, text):
+def send(subject, content, to=None, html=False, images=[]):
     addr = 'benthayer2365@gmail.com'
 
     msg = MIMEMultipart()
 
     msg['From'] = addr
-    msg['To'] = addr
+    if to is None:
+        to = addr
+    msg['To'] = to
     msg['Date'] = formatdate(localtime=True)
 
     msg['Subject'] = subject
+    
+    if html:
+        with open(content) as content_file:
+            msg.attach(MIMEText(content_file.read(), 'html'))
+    else:
+        msg.attach(MIMEText(content))
 
-    msg.attach(MIMEText(text))
+    for image_path in images:
+        with open(image_path, 'rb') as image:
+            mime_image = MIMEImage(image.read())
+        mime_image.add_header('Content-ID', '<' + image_path + '>')
+        msg.attach(mime_image)
 
     s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     s.ehlo()
     s.login(addr, os.getenv('EMAIL_PASSWORD'))
-    s.sendmail(addr, [addr], msg.as_string())
+    s.sendmail(addr, [to], msg.as_string())
     print('Sent email')
     s.close()
